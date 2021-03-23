e5ebfed5601f5d6ddee9759452ddc7cef5e2435a
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-03-23 23:57:23 +0530

    Matrix chain multiplication

--- matrix-multiplication/matmul.c
+++ matrix-multiplication/matmul.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define DEFAULT_ROW 1001
+#define DEFAULT_COLUMN 1001
 typedef struct Matrix {
-	int matrix[1001][1001];
+	int matrix[DEFAULT_ROW][DEFAULT_COLUMN];
 } Matrix;
 
 
@@ -73,6 +75,97 @@ void print_all_matrix(Matrix *restrict all_matrix[],int D[],int n)
     }
 }
 
+Matrix * multiply_matrix(Matrix * a, Matrix * b,int p,int q, int r)
+{    Matrix *result;
+    result=malloc(sizeof(Matrix));
+    /*
+    size of matrix a is pxq
+    size of matrix b is qxr
+    Maximum value of p,q or r can be 1000.
+    Your Code goes here. The output of their matrix multiplication
+    should be stored in result and returned. Just code this function,
+    no need to write anything in main(). This function will be called directly.
+    We are dealing with pointers so use result->matrix and not result.matrix
+    Also note you can write any other function that you might need.
+    */
+
+#define N 1000
+
+    int stridei = 20, stridek = 512, stridej = 20, i, j, k, gotill;
+    register int *arow;
+    register int *brow;
+    register int ii, jj, kk, tmp;
+
+    for(ii = 0; ii < p; ii++){
+        for(jj = 0; jj < r; jj++){
+            result->matrix[ii][jj] = 0;
+        }
+    }
+
+#define min(x, y) (y^((x^y) & -(x<y)))
+
+    for(i = 0; i < N; i++){
+        for(j = 0; j < i; j++){
+            tmp = b->matrix[i][j];
+            b->matrix[i][j] = b->matrix[j][i];
+            b->matrix[j][i] = tmp;
+        }
+    }
+
+    for(i = 0; i < p; i += stridei){
+        for(j = 0; j < r; j += stridej){
+            for(k = 0; k < q; k += stridek){
+                for(ii = i; ii < min(i + stridei, p); ii++){
+                    arow = a->matrix[ii];
+                    for(jj = j; jj < min(j + stridej, r); jj++){
+                        gotill = min(k + stridek, q);
+                        tmp = 0;
+                        brow = b->matrix[jj];
+                        for(kk = k; kk + 15 < gotill; kk+=16){
+                            tmp += arow[kk + 0] * brow[kk + 0]
+                                +  arow[kk + 1] * brow[kk + 1]
+                                +  arow[kk + 2] * brow[kk + 2]
+                                +  arow[kk + 3] * brow[kk + 3]
+                                +  arow[kk + 4] * brow[kk + 4]
+                                +  arow[kk + 5] * brow[kk + 5]
+                                +  arow[kk + 6] * brow[kk + 6]
+                                +  arow[kk + 7] * brow[kk + 7]
+                                +  arow[kk + 8] * brow[kk + 8]
+                                +  arow[kk + 9] * brow[kk + 9]
+                                +  arow[kk + 10] * brow[kk + 10]
+                                +  arow[kk + 11] * brow[kk + 11]
+                                +  arow[kk + 12] * brow[kk + 12]
+                                +  arow[kk + 13] * brow[kk + 13]
+                                +  arow[kk + 14] * brow[kk + 14]
+                                +  arow[kk + 15] * brow[kk + 15];
+                        }
+                        while(kk < gotill){
+                            tmp += arow[kk] * brow[kk];
+                            kk++;
+                        }
+                        result->matrix[ii][jj] += tmp;
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
+
+Matrix * matrix_chain_multiplication(int si,int ei,int n,Matrix *restrict all_matrix[],int D[n+1],long long int s[n+1][n+1])
+{
+    if (si==ei)
+        return all_matrix[si-1];
+
+    int ret_matrix_row = D[si-1];
+    int ret_matrix_col = D[ei];
+
+    int pi = s[si][ei];
+
+    return multiply_matrix(matrix_chain_multiplication(si,pi,n,all_matrix,D,s),matrix_chain_multiplication(pi+1,ei,n,all_matrix,D,s),ret_matrix_row,D[si],ret_matrix_col);
+}
+
+
 int main()
 {
     int n;
@@ -119,7 +212,8 @@ int main()
     //     printf("\n");
     // }
 
-    
+    Matrix * answer = matrix_chain_multiplication(1,n,n,all_matrix,D,s);
+    print_matrix(answer,D[0],D[n]);
 
     return 0;
 }
